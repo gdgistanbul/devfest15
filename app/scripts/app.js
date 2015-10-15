44fd671d0940db8fa9b869a26e891ccee8e7a053
Said Tahsin Dane <tasomaniac@gmail.com>
2015-10-15 15:59:07 +0300

    Initial website

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,7 +22,6 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     // Listen for template bound event to know when bindings
     // have resolved and content has been stamped to the page
     app.addEventListener('dom-change', function() {
-        console.log('Hello, folks! It is Hoverboard by GDG Lviv. Contact Oleh Zasadnyy for more details.');
     });
 
     // See https://github.com/Polymer/polymer/issues/1381
